ddb82b522a3143b462eb740f2e5c23373036e45d
Hae Won <cgodnjs8@gmail.com>
2021-07-13 09:26:10 +0900

    dd

---
+++ js/includeHTML.js
@@ -0,0 +1,36 @@
+// $(function includeHTML(divContainer, urlHTML){
+//     let xhttp = new XMLHttpRequest();
+
+//     xhttp.onreadystatechange = function (){
+//         if (this.readyState ==  4) {
+//             if(this.status == 200) {divContainer.includeHTML = this.responseText;}
+//             if(this.status == 400) {divContainer.innerHtml = "Page Not Found.";}
+//         }
+//     }
+
+//     xhttp.open("GET", urlHTML, true);
+//     xhttp.send();
+// });
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+
+    z = document.getElementsByTagName("*");
+    for(i = 0; i < z.length; i++){
+        elmnt = z[i];
+        file = elmnt.getAttribute("include-html");
+        if(file){
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if(this.readyState == 4 && this.status == 200){
+                    elmnt.innerHTML = this.responseText;
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
