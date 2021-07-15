004c896aca0bfba1ed63ca4dd20f545c031ceec5
Hae Won <cgodnjs8@gmail.com>
2021-07-15 14:51:34 +0900

    contact us 작성중

--- contact/contact.php
+++ contact/contact.php
@@ -58,21 +58,21 @@
                     print_r($_REQUEST); // print_r(); 배열 내용 찍기 --> $_REQUEST == form 내용 파라미터 배열
                     exit(); // 전송 눌렀을 때 파라미터 표시하고 정지 (뒤로가기로 돌아가)
 
-                    // 파라미터 name이랑 $_REQUEST['키'] 맞춰주기 (지금 안 맞음)
+                    // 파라미터 name이랑 $_REQUEST['키'] 맞춰주기
                     $admin_email = "eeyore@workinglabs.kr"; //받는사람
-                    $subject = $_REQUEST['subject'] ?? ''; //문의유형 이것도 없는데!
+                    $subject = $_REQUEST['subject'] ?? ''; //문의유형
                     $kname = $_REQUEST['kname']; //이름
                     $email = $_REQUEST['email']; //이메일
                     $contact = $_REQUEST['contact']; //연락처
                     $bname = $_REQUEST['bname'] ?? ''; //사업장명
                     $bnum = $_REQUEST['bnum'] ?? ''; //사업장 번호
                     $text = $_REQUEST['text']; //상세내용
-                    echo "이메일 전송 완료";
                                        
                     if(!filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
                         // mail("To : " . $admin_email, "From : " . $email, "문의유형 : " . $subject, "이름 : " . $name, "연락처 : " . $contact, "사업장명 : " . $company_name, "사업장번호 : " . $company_num, "문의내용 : " . $message); //send mail
                         $rs = mail('eeyore@workinglabs.kr', $subject, $text); //send mail
                         // echo "Thank you for your message!";
+                        
                         if ($rs) {
                             $result = '메일이 전송되었습니다?';
                         } else {
@@ -94,7 +94,7 @@
                     <ul class="input_in">
                         <li class="op">
                             문의유형&#42;
-                            <select class="ipt-select" required_name = "subject">
+                            <select class="ipt-select" required_name = "subject" name="subject">
                                 <option value="기업도입문의" selected>기업도입문의</option>
                                 <option value="서비스문의">서비스문의</option>
                                 <option value="제휴문의">제휴문의</option>
